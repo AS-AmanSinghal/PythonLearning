c5b721c1c7e1f8d489375a15bb21965df20e1a84
Aman Singhal <amansinghal.it1@gmail.com>
2021-03-31 01:09:40 +0530

    Second and Third

---
+++ SecondProgram.py
@@ -0,0 +1,23 @@
+# type() is used to find the type of passed argument in that.
+
+print(type(5 + 3))
+print(type(5 - 3))
+print(type(5 * 3))
+print(type(5 / 4))
+
+# math fuction
+
+print(round(3.9))
+print(round(3.1))
+print(round(2.87654, 2))
+
+# operator precedence
+
+print((20 + 3) + 2 ** 2)
+
+# precedence order:-
+
+# ()
+# **
+# / and *
+# + and -
---
+++ ThirdProgram.py
@@ -0,0 +1,33 @@
+# str (stands for String):-
+
+# 1. use single or double quotes for single line string
+
+print("Hello World")
+print('Hello World')
+
+# 2. use 3 single or double quotes for multi line string
+
+print('''
+Hello
+World
+''')
+
+print("""
+Hello
+World
+""")
+
+# string concatination
+
+print("Hello" + " " + "World")
+
+print("Hello " * 5)
+
+# type conversion
+
+print(str(100))
+print(type(int(str(100))))
+
+# escape sequence
+
+print("It\'s raining today. ")
